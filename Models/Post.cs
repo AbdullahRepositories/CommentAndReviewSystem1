0edb7fdc198be205961d3cd3e116527a9ee7b5a9
abdullah <abdullah@E-LENOVO-LAPTOP>
2024-08-29 14:03:42 +0300

    intitial commit

---
+++ Models/Post.cs
@@ -0,0 +1,22 @@
+﻿namespace CommentAndReviewSystem1.Models
+{
+    public partial class Post
+    {
+        public Post() { }
+
+        public int PostId { get; set; }
+        public int CategoryId { get; set; }
+        public string? UserId { get; set; }
+        public DateTime AddingDate { get; set; } = DateTime.Now;
+
+        public double? AverageRating { get; set; } = 0.0;
+
+        public bool Status { get; set; }
+
+        public string Content { get; set; }
+        public virtual ICollection<Review> Reviews { get; set; } = new HashSet<Review>();
+
+
+
+    }
+}
---
+++ Models/Review.cs
@@ -0,0 +1,20 @@
+﻿namespace CommentAndReviewSystem1.Models
+{
+    public partial class Review
+    {
+        public Review() { }
+
+        public int ReviewId { get; set; }
+        public string Description { get; set; }
+        public bool Status { get; set; }
+        public byte Rating { get; set; }
+
+        public DateTime ReviewingDate { get; set; } = DateTime.Now;
+        public string UserId { get; set; }
+        public string PostId { get; set; }
+        public virtual Post Post { get; set; }
+        //public virtual User User { get; set; }
+
+
+    }
+}
